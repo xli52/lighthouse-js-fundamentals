a3c92f817e5b3b978b8099a3bae452888b60f915
Xiang Li <lxmichael52@gmail.com>
2022-04-03 12:50:47 -0700

    conditionals.js file

---
+++ conditionals.js
@@ -0,0 +1,19 @@
+const raining = true;
+const cold = false;
+const temperature = 12;
+
+if (raining) {
+  console.log('Don\'t forget your umbrella!');
+}
+
+if (cold) {
+  console.log('Make sure you pick out a scarf!');
+} else if (temperature < 15) {
+  console.log('Short sleeves are fine.');
+} else {
+  console.log('Short sleeves are fine.');
+}
+
+console.log('Now you\'re ready to go outside!');
+
+
