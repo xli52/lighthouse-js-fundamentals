7955cfc52ec973bdff79fc138f91bd64d2672335
Xiang Li <lxmichael52@gmail.com>
2022-04-16 14:38:52 -0700

    add new files

--- hello.js
+++ hello.js
@@ -2,3 +2,10 @@ const sayHello = function (name) {
   console.log('hello, ' + name);
 }
 sayHello('Xiang');
+
+["Mal",
+"Zoe",
+"Wash",
+"Inara",
+"Jayne",
+"Kaylee"]
---
+++ ice-cream.js
@@ -0,0 +1,15 @@
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+iceCreamFlavours.push('root beer');
+
+let sum = 3 + 2;
+
+const doubleSum = function(a, b) {
+  sum = a + b;
+  return sum * 2;
+}
+
+doubleSum(5, 4);
+
+console.log(sum);
